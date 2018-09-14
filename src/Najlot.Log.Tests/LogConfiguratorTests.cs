f902464b3769869d96f75c4f3204c3437da7cc7c
najlot <github.najlot@gmail.com>
2018-09-14 21:47:26 +0200

    There is not that much to test

---
+++ src/Najlot.Log.Tests/LogConfiguratorTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace Najlot.Log.Tests
+{
+	public class LogConfiguratorTests
+	{
+		[Fact]
+		public void LogConfiguratorInstanceMustNotBreak()
+		{
+			LogConfigurator.Instance
+				.GetLogConfiguration(out var logConfiguration)
+				.AddConsoleLogDestination()
+				.GetLoggerPool(out var loggerPool);
+
+			var log = loggerPool.GetLogger("default");
+
+			log.Fatal("Hello, World!");
+		}
+	}
+}
