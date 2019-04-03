79c40d6feaabd2efe7e0b3f136bcdd76c2ac95d4
najlot <najlot@users.noreply.github.com>
2019-04-03 22:11:19 +0200

    unit tests

--- src/Najlot.Log.Tests/FilterMiddlewareTests.cs
+++ src/Najlot.Log.Tests/FilterMiddlewareTests.cs
@@ -55,5 +55,65 @@ namespace Najlot.Log.Tests
 
 			Assert.True(File.Exists(fileName));
 		}
+
+		[Fact]
+		public void FilterMiddlewareGetsCorrectType()
+		{
+			bool loggedToFirst = false;
+			bool loggedToSecond = false;
+
+			var log = LogAdminitrator
+				.CreateNew()
+				.AddCustomDestination(new LogDestinationMock(msg =>
+				{
+					loggedToFirst = true;
+				}))
+				.AddCustomDestination(new SecondLogDestinationMock(msg =>
+				{
+					loggedToSecond = true;
+				}))
+				.SetFilterMiddleware<DenyLogDestinationMockFilterMiddleware>()
+				.GetLogger("");
+
+			log.Fatal("TEST!");
+
+			Assert.False(loggedToFirst);
+			Assert.True(loggedToSecond);
+		}
+
+		[Fact]
+		public void LoggerShouldNotDieBecauseOfFilterMiddleware()
+		{
+			var fileName = nameof(LoggerShouldNotDieBecauseOfFilterMiddleware) + ".log";
+			
+			if (File.Exists(fileName))
+			{
+				File.Delete(fileName);
+			}
+
+			var logAdminitrator = LogAdminitrator
+				.CreateNew()
+				.SetLogLevel(LogLevel.Trace)
+				.AddFileLogDestination(fileName, keepFileOpen: false)
+				.SetFilterMiddleware<DenyAllFilterMiddleware>();
+
+			var log = logAdminitrator.GetLogger("");
+
+			log.Debug("TEST 1!");
+
+			Assert.False(File.Exists(fileName));
+
+			logAdminitrator.SetFilterMiddlewareByType(null);
+
+			log.Debug("TEST 2!");
+
+			Assert.False(File.Exists(fileName));
+
+			logAdminitrator.SetFilterMiddlewareByType(typeof(string));
+
+			log.Debug("TEST 3!");
+
+			Assert.False(File.Exists(fileName));
+		}
 	}
 }
---
+++ src/Najlot.Log.Tests/Mocks/FilterMiddlewareMock.cs
@@ -0,0 +1,13 @@
+﻿using Najlot.Log.Middleware;
+using System;
+
+namespace Najlot.Log.Tests.Mocks
+{
+	public sealed class DenyLogDestinationMockFilterMiddleware : IFilterMiddleware
+	{
+		public bool AllowThrough(Type destinationType, LogMessage message)
+		{
+			return destinationType != typeof(LogDestinationMock);
+		}
+	}
+}
