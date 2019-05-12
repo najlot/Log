e5d051eaab74736f05fb8748fbb9a973049887e8
najlot <najlot@users.noreply.github.com>
2019-05-12 18:14:48 +0200

    json formatting middleware

---
+++ src/Najlot.Log.Tests/JsonFormattingMiddlewareTests.cs
@@ -0,0 +1,72 @@
+﻿using Najlot.Log.Middleware;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace Najlot.Log.Tests
+{
+	public class JsonFormattingMiddlewareTests
+	{
+		private readonly JsonFormatMiddleware _middleware = new JsonFormatMiddleware();
+
+		[Fact]
+		public void MiddlewareMustProduceValidJson()
+		{
+			var message = new LogMessage(
+				DateTime.Parse("2019-05-12T18:02:50.6571583+02:00"),
+				LogLevel.Info,
+				typeof(LogMessage).FullName,
+				null,
+				"some stuff happened {count:D3} times",
+				null,
+				new List<KeyValuePair<string, object>>
+				{
+					new KeyValuePair<string, object>("count", 10),
+					new KeyValuePair<string, object>("count", 10)
+				}
+			);
+
+			var expected = "{\"DateTime\":\"2019-05-12T18:02:50.6571583+02:00\",\"LogLevel\":2,\"Category\":\"Najlot.Log.LogMessage\",\"State\":,\"BaseMessage\":\"some stuff happened {count:D3} times,\"Message\":\"some stuff happened 010 times\",\"Exception\":null,\"ExceptionIsValid\":false,\"Arguments\":[{\"Key\":\"count\",\"Value\":10},{\"Key\":\"count\",\"Value\":10}]}";
+			var actual = _middleware.Format(message);
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void ExceptionMustBeLogged()
+		{
+			Exception exc = null;
+
+			try
+			{
+				throw new Exception("No CD-ROM / cup holder avaible"); ;
+			}
+			catch(Exception ex)
+			{
+				exc = ex;
+			}
+
+			var message = new LogMessage(
+				DateTime.Parse("2019-05-12T18:02:50.6571583+02:00"),
+				LogLevel.Info,
+				typeof(LogMessage).FullName,
+				null,
+				"some stuff happened {count:D3} times",
+				exc,
+				new List<KeyValuePair<string, object>>
+				{
+					new KeyValuePair<string, object>("count", 10),
+					new KeyValuePair<string, object>("count", 10)
+				}
+			);
+
+			var actual = _middleware.Format(message);
+
+			var expectedToFind = "No CD-ROM / cup holder avaible";
+			Assert.True(actual.IndexOf(expectedToFind) != -1);
+
+			expectedToFind = "\"ExceptionIsValid\":true";
+			Assert.True(actual.IndexOf(expectedToFind) != -1);
+		}
+	}
+}
---
+++ src/Najlot.Log/Middleware/JsonFormatMiddleware.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Najlot.Log.Middleware
+{
+	/// <summary>
+	/// Serialises LogMessage to a single line json string.
+	/// </summary>
+	public class JsonFormatMiddleware : IFormatMiddleware
+	{
+		private void AppendJson(StringBuilder sb, string raw)
+		{
+			// https://www.freeformatter.com/json-escape.html
+
+			if (string.IsNullOrEmpty(raw))
+			{
+				return;
+			}
+
+			foreach (char c in raw)
+			{
+				switch (c)
+				{
+					case '\b':
+						sb.Append("\\b");
+						break;
+
+					case '\f':
+						sb.Append("\\f");
+						break;
+
+					case '\n':
+						sb.Append("\\n");
+						break;
+
+					case '\r':
+						sb.Append("\\r");
+						break;
+
+					case '\t':
+						sb.Append("\\t");
+						break;
+
+					case '"':
+						sb.Append("\\\"");
+						break;
+
+					case '\\':
+						sb.Append("\\\\");
+						break;
+					
+					default:
+						if (c < ' ')
+						{
+							sb.Append("\\u");
+							sb.Append(((int)c).ToString("X").PadLeft(4, '0'));
+						}
+						else
+						{
+							sb.Append(c);
+						}
+						break;
+				}
+			}
+		}
+
+		private void SerializeArgument(StringBuilder sb, KeyValuePair<string, object> arg)
+		{
+			sb.Append("{\"Key\":\"");
+			AppendJson(sb, arg.Key.ToString());
+			sb.Append("\",\"Value\":");
+			AppendJson(sb, arg.Value?.ToString());
+			sb.Append('}');
+		}
+
+		public string Format(LogMessage message)
+		{
+			var sb = new StringBuilder();
+
+			sb.Append("{\"DateTime\":\"");
+			sb.Append(message.DateTime.ToString("o"));
+			sb.Append("\",\"LogLevel\":");
+			sb.Append(((int)message.LogLevel).ToString());
+			sb.Append(",\"Category\":\"");
+			AppendJson(sb, message.Category);
+			sb.Append("\",\"State\":");
+			AppendJson(sb, message.State?.ToString());
+			sb.Append(",\"BaseMessage\":\"");
+			AppendJson(sb, message.Message);
+			sb.Append(",\"Message\":\"");
+			AppendJson(sb, LogArgumentsParser.InsertArguments(message.Message, message.Arguments));
+			sb.Append("\",\"Exception\":");
+
+			if (message.ExceptionIsValid)
+			{
+				sb.Append("\"");
+				AppendJson(sb, message.Exception.ToString());
+				sb.Append("\",\"ExceptionIsValid\":true");
+			}
+			else
+			{
+				sb.Append("null,\"ExceptionIsValid\":false");
+			}
+
+			sb.Append(",\"Arguments\":[");
+
+			bool isFirst = true;
+
+			foreach (var arg in message.Arguments)
+			{
+				if (isFirst) isFirst = false;
+				else sb.Append(',');
+
+				SerializeArgument(sb, arg);
+			}
+			
+			sb.Append("]}");
+
+			return sb.ToString();
+		}
+	}
+}
